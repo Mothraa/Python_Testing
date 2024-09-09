1d9ffbd3adeb6b34896666ced10b6edec2bc2fcc
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-09-09 16:17:30 +0200

    return after closing file

--- main_app/services.py
+++ main_app/services.py
@@ -5,11 +5,11 @@ import json
 
 def load_club_data():
     with open('clubs.json') as f:
-         clubs_list = json.load(f)['clubs']
-         return clubs_list
+        clubs_list = json.load(f)['clubs']
+    return clubs_list
 
 
 def load_competition_data():
     with open('competitions.json') as f:
-         competitions_list = json.load(f)['competitions']
-         return competitions_list
+        competitions_list = json.load(f)['competitions']
+    return competitions_list
