7e27611e484fac13423f75472b5c10c62647eef9
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-09-09 09:17:54 +0200

    remove examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,26 @@
 import pytest
 
-from main_app import create_app
+# from main_app import create_app
 
-@pytest.fixture
-def first_fixture():
-    data = {"first_name": "Ranga",
-            "name": "Gonnage"}
-    return data
+# @pytest.fixture
+# def first_fixture():
+#     data = {"first_name": "Ranga",
+#             "name": "Gonnage"}
+#     return data
 
-@pytest.fixture
-def client():
-    app = create_app({"TESTING": True})
-    with app.test_client() as client:
-        yield client
+# @pytest.fixture
+# def client():
+#     app = create_app({"TESTING": True})
+#     with app.test_client() as client:
+#         yield client
 
 
-class TestClass:
-    def setup_method(self, method):
-        print("--> Setup method")
+# class TestClass:
+#     def setup_method(self, method):
+#         print("--> Setup method")
 
-    def teardown_method(self, method):
-        print("\n--> Teardown method")
+#     def teardown_method(self, method):
+#         print("\n--> Teardown method")
 
-    def test_one(self):
-        print("--> Run first test")
+#     def test_one(self):
+#         print("--> Run first test")
--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,11 +1,11 @@
 from locust import HttpUser, task
 
+## example
+# class ProjectPerfTest(HttpUser):
+#     @task
+#     def home(self):
+#         self.client.get("/home")
 
-class ProjectPerfTest(HttpUser):
-    @task
-    def home(self):
-        self.client.get("/home")
-
-    @task(3)
-    def login(self):
-        response = self.client.get("/user/login")
+#     @task(3)
+#     def login(self):
+#         response = self.client.get("/user/login")
