4e7d2ff040b75d67619c7ff27bd8b22478b22576
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-09-16 22:51:45 +0200

    fix #issue-650528764

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,41 +1,54 @@
-from flask import Blueprint, render_template, request, redirect, flash, url_for
+from flask import Blueprint, render_template, request, redirect, flash, url_for, g
 
 bp = Blueprint('main', __name__)
 
-#TODO : regarder url_for qui est chargé pour le logout => a priori pour pointer sur un path lors de redirection
-# TODO : authentification ?
-# TODO : CSRF token ? autres aspects sécurité ?
+# TODO : regarder url_for qui est chargé pour le logout => a priori pour pointer sur un path lors de redirection
+# TODO : authentification au lieu d'une simple identification, CSRF token, Flask-Login pour les permissions,...
+# autres aspects sécurité ?
+
 
 @bp.route('/')
 def index():
     return render_template('index.html')
 
 
-@bp.route('/showSummary',methods=['POST'])
+@bp.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club_by_email_list = [club for club in g.clubs if club['email'] == request.form['email']]
+
+    if len(club_by_email_list) == 0:
+        flash("Email not found")
+        print("DEBUG")
+        return redirect(url_for('main.index'))
+
+    if len(club_by_email_list) > 1:
+        flash("Email found for {len(clubs_by_email)} clubs. Please contact administrator.")
+        return redirect(url_for('main.index'))
+
+    # cas nominal d'un seul club trouvé pour une adresse
+    club = club_by_email_list[0]
+    return render_template('welcome.html', club=club, competitions=g.competitions)
 
 
 @bp.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+def book(competition, club):
+    foundClub = [c for c in g.clubs if c['name'] == club][0]
+    foundCompetition = [c for c in g.competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html',club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=g.competitions)
 
 
-@bp.route('/purchasePlaces',methods=['POST'])
+@bp.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
+    competition = [c for c in g.competitions if c['name'] == request.form['competition']][0]
+    club = [c for c in g.clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=g.competitions)
 
 
 # TODO: Add route for points display
@@ -43,4 +56,4 @@ def purchasePlaces():
 
 @bp.route('/logout')
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,32 @@
 # memo : pas besoin d'importer les fixtures présentes dans conftest, pytest s'en charge !
+# TODO : validation format email : (email-validator ou pyIsEmail ou par regex) en complément de la verif de base du template
 
-def test_index(client):
+
+def test_index_status_code(client):
     response = client.get('/')
     assert response.status_code == 200
+
+
+def test_index_read_content(client):
+    """ Check if we are on the right page """
+    response = client.get('/')
     assert b'GUDLFT Registration' in response.data
+
+
+def test_no_email(client):
+    # POST avec le champ email vide et on suit la redirection
+    response = client.post('/showSummary', data={'email': ''}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Email not found' in response.data
+
+
+def test_email_valid(client, mock_clubs):
+    response = client.post('/showSummary', data={'email': 'kate@shelifts.co.uk'})
+    assert response.status_code == 200
+    assert b'Welcome, kate@shelifts.co.uk' in response.data
+
+
+def test_email_not_valid(client):
+    response = client.post('/showSummary', data={'email': 'pipo@mail.com'}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Email not found' in response.data
