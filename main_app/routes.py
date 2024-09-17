485bdb10456a3ec5613d80e7bd4f3e712dda9a20
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-09-17 17:23:22 +0200

    fix #issue-650528997

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, session, render_template, request, redirect, flash, url_for, g
 
+from .services import BookingService
+
 bp = Blueprint('main', __name__)
 
 # TODO : regarder url_for qui est chargé pour le logout => a priori pour pointer sur un path lors de redirection
-# TODO : authentification au lieu d'une simple identification, session flask, CSRF token, Flask-Login pour les permissions,...
-# autres aspects sécurité ?
+# TODO : authentification au lieu d'une simple identification, session flask, CSRF token (flask-WTF),
+# TODO : Flask-Login pour les permissions,... autres aspects sécurité ?
 
 
 @bp.route('/')
@@ -15,6 +17,7 @@ def index():
 @bp.route('/showSummary', methods=['POST'])
 def showSummary():
     club_by_email_list = [club for club in g.clubs if club['email'] == request.form['email']]
+    # TODO : interdire la reservation de places pour des compétitions qui ont déjà eu lieu
 
     if len(club_by_email_list) == 0:
         flash("Email not found")
@@ -34,7 +37,7 @@ def book(competition, club):
     foundClub = [c for c in g.clubs if c['name'] == club][0]
     foundCompetition = [c for c in g.competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub, competition=foundCompetition)
+        return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=g.competitions)
@@ -42,10 +45,28 @@ def book(competition, club):
 
 @bp.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in g.competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in g.clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    booking_service = BookingService(g.clubs, g.competitions)
+    # competition = [c for c in g.competitions if c['name'] == request.form['competition']][0]
+    # club = [c for c in g.clubs if c['name'] == request.form['club']][0]
+
+    club = booking_service.get_club_by_name(request.form['club'])
+    competition = booking_service.get_competition_by_name(request.form['competition'])
+
+    try:
+        places_required = int(request.form.get('places', ''))
+    except ValueError:
+        flash('Invalid number of places')
+        return render_template('welcome.html', club=club, competitions=g.competitions)
+
+    # On vérifie que la competition a suffisamment de places
+    if not booking_service.has_enough_places(competition, places_required):
+        flash(f"Not enough places in competition ({competition['numberOfPlaces']}) to book {places_required} places.")
+        return render_template('booking.html', club=club, competition=competition)
+
+
+    # competition['numberOfPlaces'] = competition['numberOfPlaces'] - placesRequired
+
+    # TODO update JSON file
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=g.competitions)
 
