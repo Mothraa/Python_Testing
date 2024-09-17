b792f880478275b3ef10d95ad74050e212d6f0d5
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-09-17 23:59:41 +0200

    add statut for competitions in view

--- main_app/routes.py
+++ main_app/routes.py
@@ -27,9 +27,20 @@ def showSummary():
         flash(f"Email found for {len(club_by_email_list)} clubs. Please contact administrator.")
         return redirect(url_for('main.index'))
 
+    
     # cas nominal d'un seul club trouvé pour une adresse
     club = club_by_email_list[0]
-    return render_template('welcome.html', club=club, competitions=g.competitions)
+
+    # ajout du statut "is_competition_in_future" pour chaque compétition.
+    booking_service = BookingService(g.clubs, g.competitions)
+    competitions_with_future_status = []
+    for comp in g.competitions:
+        competition_with_status = dict(comp)
+        competition_with_status['is_competition_in_future'] = booking_service.is_competition_in_future(comp)
+        competitions_with_future_status.append(competition_with_status)
+
+    return render_template('welcome.html', club=club, competitions=competitions_with_future_status)
+    # return render_template('welcome.html', club=club, competitions=g.competitions)
 
 
 @bp.route('/book/<competition>/<club>')
